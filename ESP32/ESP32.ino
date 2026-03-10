e25edabc6ffa2f36f8ab55308d853b6af9d6856f
Carmi Zadok <carmish144@gmail.com>
2026-03-10 20:50:47 +0200

    Advertisment is working. Implemented build_packet function. packet still hardcoded. need to fix that.

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -10,20 +10,7 @@
 
 #define BLE_PACKET_SIZE 37 
 
-char* packet_data = (char*)malloc(sizeof(char)*37);
-*packet_data = {
-  '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', // BLE MAC Address (p[0] | 0b11 << 6 || p[1..5] where p is public key)
-  '\x1e', //Payload Length in Bytes (30)
-  '\xff', // Advertisment Type
-  '\x00', '\x4c', // Company ID (Apple)
-  '\x12', // OF Type
-  '\x19', // OF Data Length in Bytes (25)
-  '\x00', // Status (Battery Level)
-  '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', // Public Key Bytes p[6..27]
-  '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00',
-  '\x00', // Public Key Bits p[0] >> 6
-  '\x00' // Hint (0x00 on iOS Reports)
-};
+// char* packet_data = (char*)malloc(sizeof(char)*37);
 
 static int RNG(uint8_t *dest, unsigned size) {
   // Use the least-significant bits from the ADC for an unconnected pin (or connected to a source of 
@@ -66,8 +53,42 @@ void print_key(uint8_t* key, int size){
   Serial.println();
 }
 
-void build_packet_data(const char* packet){
-  // Need to Implement.
+// *packet_data = {
+//   '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', // BLE MAC Address (p[0] | 0b11 << 6 || p[1..5] where p is public key)
+//   '\x1e', //Payload Length in Bytes (30)
+//   '\xff', // Advertisment Type
+//   '\x00', '\x4c', // Company ID (Apple)
+//   '\x12', // OF Type
+//   '\x19', // OF Data Length in Bytes (25)
+//   '\x00', // Status (Battery Level)
+//   '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', // Public Key Bytes p[6..27]
+//   '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00',
+//   '\x00', // Public Key Bits p[0] >> 6
+//   '\x00' // Hint (0x00 on iOS Reports)
+// };
+
+void build_packet_data(char* packet){
+  // BLE MAC Address (p[0] | 0b11 << 6 || p[1..5] where p is public key)
+  for (int i = 0; i < 6; i++){
+    packet[i] = '\x00';
+  }
+
+  packet[6] = '\x1e'; // Payload Length in Bytes (30)
+  packet[7] = '\xff'; // Advertisment Type
+  // Company ID (Apple)
+  packet[8] = '\x00';
+  packet[9] = '\x4c'; 
+  packet[10] = '\x12'; // OF Type
+  packet[11] = '\x19'; // OF Data Length in Bytes (25)
+  packet[12] = '\x00'; // Status (Battery Level)
+  
+  // Public Key Bytes p[6..27]
+  for (int i = 13; i < 13+22; i++){
+    packet[i] = '\x00';
+  }
+  
+  packet[35] = '\x00'; // Public Key Bits p[0] >> 6
+  packet[36] = '\x00'; // Hint (0x00 on iOS Reports)
 }
 
 void setup() {
@@ -105,17 +126,19 @@ void setup() {
   dbg_print("Initializing BLE Adverisment.");
 
   BLEDevice::init("");
-
+  dbg_print("BLEDevice initialized.");
   BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
-
+  dbg_print("BLEAdvertising initialized.");
   char* packet_data = (char*)malloc(sizeof(char)*37);
+  dbg_print("packet_data memory allocated.");
   build_packet_data(packet_data);
-
-  BLEAdvertisementData advData;
-  advData.addData(packet_data);
+  dbg_print("packet_data built.");
+  BLEAdvertisementData* advData = (BLEAdvertisementData*)malloc(sizeof(BLEAdvertisementData));
+  advData->addData(packet_data);
+  dbg_print("Data added to advData.");
 
   
-  pAdvertising->setAdvertisementData(advData);
+  pAdvertising->setAdvertisementData(*advData);
   pAdvertising->start();
 
   dbg_print("Started Advertising.");
